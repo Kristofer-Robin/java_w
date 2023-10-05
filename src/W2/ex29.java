f671540035e826d9d3a7da0cd477c96baa4d832a
Kristofer-Robin <kristofer-robin.lai@voco.ee>
2023-10-05 09:50:31 +0300

    Java w2, ex 26 and 29 done

---
+++ src/W2/ex29.java
@@ -0,0 +1,9 @@
+package W2;
+
+public class ex29 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
